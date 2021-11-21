2ade47a6b326bbd526bd68458aea13ca2dc80d9a
Sylvain Gotowy <sylvain.gotowy@gmail.com>
2021-11-21 14:53:22 +0100

    add resources folder

--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 
+	"image/color"
 	_ "image/png"
 )
 
 // Game implements ebiten.Game interface.
 type Game struct{}
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("resources/l.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
@@ -21,13 +33,15 @@ func (g *Game) Update() error {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.NRGBA{0xff, 0xff, 0xff, 0xff})
+	screen.DrawImage(img, &ebiten.DrawImageOptions{})
 	// Write your game's rendering.
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidthx, screenHeight int) {
+	return 640, 480
 }
 
 func main() {
